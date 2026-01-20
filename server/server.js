5df0a4e3160e2e5fd654a9a0a0ff8178c54cff95
Francis Ge <francisge527@gmail.com>
2026-01-20 20:20:53 +0800

    add routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 import express from "express";
+import landlordRoutes from "./routes/landlord.js";
 
 const app = express();
 
+//middlewares
+app.use(express.json());
+
+//sign up route
+app.use("/auth", landlordRoutes);
+
 //default route for checking
-app.get("/", (req, res) => res.send("Lomod Boarding House Management System WIP."));
+app.get("/", (req, res) =>
+  res.send("Lomod Boarding House Management System WIP."),
+);
 
 //start server
 const PORT = process.env.PORT || 3000;
