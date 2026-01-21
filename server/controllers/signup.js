d8c3e82a72c98e78b018373751bd964a2fa0ad02
Francis Ge <francisge527@gmail.com>
2026-01-21 21:35:35 +0800

    change file name from landlord.js to signup.js

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -1,5 +1,6 @@
 import crypto from "crypto";
 import { getLandlordByEmail } from "../models/landlord.js";
+import { storeEmailVerificationToken } from "../models/emailVerificationToken.js";
 import transporter from "../config/mailer.js";
 
 const signup = async (req, res) => {
@@ -15,17 +16,22 @@ const signup = async (req, res) => {
     const verificationToken = crypto.randomBytes(32).toString("hex");
     const expiresAt = new Date(Date.now() + 3 * 60 * 1000);
 
-    //send verification email with the code
+    //store verification token
+    await storeEmailVerificationToken(verificationToken, expiresAt);
+
+    //send verification email
+    const verificationLink = `http://localhost:3000/verify-email?token=${verificationToken}`;
     await transporter.sendMail({
       from: `"Francel Boarding House" <${process.env.SMTP_USER}>`,
       to: email,
       subject: "Verify your email",
-      text: "Hello world?", // Plain-text version of the message
-      html: "<b>Hello world?</b>", // HTML version of the message
+      html: `<h2>Welcome to Francel BHMS!</h2>
+             <p>Click the link below to verify your email. Expires in 3 minutes.</p>
+             <a href="${verificationLink}">Verify Email</a>
+      `,
     });
 
-    res.status(200).json({ message: "Verification email sent" });
-    //if verified, create and store new account
+    res.status(200).json({ message: `Verification email sent to ${email}.` });
   } catch (error) {
     console.error("Error when signing up:", error);
   }
