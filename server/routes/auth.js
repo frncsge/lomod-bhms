c0963f42eac4ba67f4c80dd4698b0ce70bee73ac
Francis Ge <francisge527@gmail.com>
2026-01-25 18:12:17 +0800

    move files

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup } from "../controllers/signup.js";
+import { signup } from "../controllers/landlord/signup.js";
 import { verifyEmail } from "../controllers/verifyEmail.js";
 
 const router = express.Router();
