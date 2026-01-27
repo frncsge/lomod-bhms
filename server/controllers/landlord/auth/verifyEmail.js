7dbbe657df235ffad42853f0e58b5d9708f0f0c3
Francis Ge <francisge527@gmail.com>
2026-01-27 22:47:08 +0800

    generate access token in sign up

--- server/controllers/landlord/auth/verifyEmail.js
+++ server/controllers/landlord/auth/verifyEmail.js
@@ -1,28 +1,35 @@
 import redisClient from "../../../config/redis.js";
+import jwt from "jsonwebtoken";
 import { storeNewLandlord } from "../../../models/landlord.js";
 
 const verifyEmail = async (req, res) => {
   const { token } = req.query;
+  const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
   try {
     //retrieve email and password from cache
     const data = await redisClient.get(token);
 
     //check if cache still exists
-    if (!data) {
+    if (!data)
       return res.status(400).json({ message: "Invalid or expired link" });
-    } else {
-      //parse data back to object
-      const { email, hashedPassword, accountName } = JSON.parse(data);
 
-      //email verified, create account
-      await storeNewLandlord(email, hashedPassword, accountName);
+    //parse data back to object
+    const { email, hashedPassword, accountName } = JSON.parse(data);
 
-      //delete token from redis
-      await redisClient.del(token);
-    }
+    //email verified, create account
+    const result = await storeNewLandlord(email, hashedPassword, accountName);
+    const { landlord_id } = result;
 
-    res.status(201).json({ message: "Sign up successful" });
+    //delete token from redis
+    await redisClient.del(token);
+
+    //create access token
+    const accessToken = jwt.sign({ sub: landlord_id }, accessTokenSecret, {
+      expiresIn: "15m",
+    });
+
+    res.status(201).json({ message: "Sign up successful", accessToken });
   } catch (error) {
     console.error("Error when verifying the landlord email:", error);
     res.status(500).json({ message: "Server error" });
