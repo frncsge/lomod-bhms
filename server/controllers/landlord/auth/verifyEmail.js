3142594142455dbb98fddb2c50f17994ed85b3e8
Francis Ge <francisge527@gmail.com>
2026-01-28 21:56:37 +0800

    store access token in httpOnly cookie

--- server/controllers/landlord/auth/verifyEmail.js
+++ server/controllers/landlord/auth/verifyEmail.js
@@ -30,9 +30,14 @@ const verifyEmail = async (req, res) => {
     });
 
     //store access token in httpOnly cookie
-    
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false, //true in prod, only for https
+      sameSite: "none", //allow cross-origin req
+      maxAge: 15 * 60 * 1000,
+    });
 
-    res.status(201).json({ message: "Sign up successful"});
+    res.status(201).json({ message: "Sign up successful" });
   } catch (error) {
     console.error("Error when verifying the landlord email:", error);
     res.status(500).json({ message: "Server error" });
