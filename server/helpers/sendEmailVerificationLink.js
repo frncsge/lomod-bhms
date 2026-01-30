2e99f3afd5a0ba61e8e777d6389bf698019d0ecd
Francis Ge <francisge527@gmail.com>
2026-01-30 21:34:29 +0800

    add helper for sending email verification link

---
+++ server/helpers/sendEmailVerificationLink.js
@@ -0,0 +1,13 @@
+import transporter from "../config/mailer";
+
+export const sendEmailVerificationLink = async (verificationLink, receiverEmail) => {
+  await transporter.sendMail({
+    from: `"Francel Boarding House" <${process.env.SMTP_USER}>`,
+    to: receiverEmail,
+    subject: "Francel BHMS Email Verification",
+    html: `<h2>Welcome to Francel BHMS!</h2>
+             <p>Click the link below to verify your email. Expires in 3 minutes.</p>
+             <a href="${verificationLink}">Verify Email</a>
+      `,
+  });
+};
