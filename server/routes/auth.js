05f435bdfe706927b00277dca220368d4b21f974
Francis Ge <francisge527@gmail.com>
2026-01-31 10:03:31 +0800

    add refresh token route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,16 @@
 import express from "express";
-import { landlordSignup } from "../controllers/auth/signup.js";
-import { verifyEmail } from "../controllers/auth/verifyEmail.js";
-import { landlordSignin } from "../controllers/auth/signin.js";
+import { landlordSignup } from "../controllers/auth/signup.controller.js";
+import { verifyEmail } from "../controllers/auth/verifyEmail.controller.js";
+import { landlordSignin } from "../controllers/auth/signin.controller.js";
 
 const router = express.Router();
 
+//landlord 
 router.get("/landlord/verify-email", verifyEmail);
 router.post("/landlord/sign-up", landlordSignup);
 router.post("/landlord/sign-in", landlordSignin);
+
+//refresh token
+router.post("/refresh");
 
 export default router;
