657eb9ade9f4869965b5e8a412fafa2ac35cc7e1
Francis Ge <francisge527@gmail.com>
2026-02-12 01:31:46 +0800

    update route names, imports

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,19 +1,16 @@
 import express from "express";
-import { landlordSignup } from "../controllers/auth/signup.controller.js";
+import { landlordSignUp } from "../controllers/auth/signup.controller.js";
 import { verifyEmail } from "../controllers/auth/emailVerification.controller.js";
-import { landlordSignin } from "../controllers/auth/signin.controller.js";
-import { getNewAccessToken } from "../controllers/auth/refresh.controller.js";
-import { signout } from "../controllers/auth/signout.controller.js";
+import { landlordSignIn } from "../controllers/auth/signin.controller.js";
+import { refreshUserSession } from "../controllers/auth/refresh.controller.js";
+import { signOut } from "../controllers/auth/signout.controller.js";
 
 const router = express.Router();
 
-//landlord auth
 router.get("/landlord/verify-email", verifyEmail);
-router.post("/landlord/sign-up", landlordSignup);
-router.post("/landlord/sign-in", landlordSignin);
-router.post("/landlord/sign-out", signout);
-
-//refresh token
-router.post("/refresh", getNewAccessToken);
+router.post("/landlord/sign-up", landlordSignUp);
+router.post("/landlord/sign-in", landlordSignIn);
+router.post("/landlord/sign-out", signOut);
+router.post("/refresh", refreshUserSession);
 
 export default router;
