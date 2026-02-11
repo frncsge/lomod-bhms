e3faaeb2c1e7542e113fcd4f513613d2426b2596
Francis Ge <francisge527@gmail.com>
2026-02-12 01:26:39 +0800

    put redis caching into a separate helper function and update response message

--- server/controllers/auth/signout.controller.js
+++ server/controllers/auth/signout.controller.js
@@ -1,18 +1,17 @@
-import redisClient from "../../config/redis.js";
-import { clearJwtCookies } from "../../utils/authCookies.util.js";
+import { clearJwtCookies } from "../../utils/cookies.util.js";
+import { delCachedRefreshToken } from "../../utils/cache.util.js";
 
-export const signout = async (req, res) => {
+export const signOut = async (req, res) => {
   try {
     const { refresh_token } = req.cookies;
 
     //delete refresh token from redis
     if (refresh_token) {
-      await redisClient.del(`refreshToken:${refresh_token}`);
+      await delCachedRefreshToken(refresh_token);
     }
 
-    //then clear the access and refresh token cookies
     clearJwtCookies(res);
-    res.status(200).json({ message: "Sign-out successful" });
+    res.status(200).json({ message: "Sign out successful" });
   } catch (error) {
     console.error("Error signing out:", error);
     res.status(500).json({ message: "Server error" });
