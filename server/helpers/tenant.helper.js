17ed127e97feec621b2e7b2e179a7446c613d1a4
Francis Ge <francisge527@gmail.com>
2026-02-17 18:51:00 +0800

    add .trim() to last and first names

--- server/helpers/tenant.helper.js
+++ server/helpers/tenant.helper.js
@@ -2,7 +2,9 @@ import crypto from "crypto";
 
 export const generateTenantUsername = (user, suffixSize = 2) => {
   const randomSuffix = crypto.randomBytes(suffixSize).toString("hex");
-  const username = `${user.lastName.trim().replace(/\s+/g, "")}.${user.firstName.trim().replace(/\s+/g, "_")}${randomSuffix}`;
+  const username = `
+  ${user.lastName.toLowerCase().trim().replace(/\s+/g, "")}.${user.firstName.toLowerCase().trim().replace(/\s+/g, "_")}${randomSuffix}
+  `;
 
   return username;
 };
