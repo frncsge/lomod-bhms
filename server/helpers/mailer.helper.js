eadf7e197ace55918fe1dd6a25db447117fa26d9
Francis Ge <francisge527@gmail.com>
2026-02-17 18:50:06 +0800

    temporary remove mailers, put token and link generation inside mailer functions

--- server/helpers/mailer.helper.js
+++ server/helpers/mailer.helper.js
@@ -1,14 +1,15 @@
 import transporter from "../config/mailer.config.js";
-import { cacheVerificationToken } from "../utils/cache.util.js";
+import { cacheVerificationToken, cacheSetPasswordToken } from "../utils/cache.util.js";
+import { generateRandomToken } from "./tokens.helper.js";
 
-export const sendEmailVerificationLink = async (email, user) => {
+export const sendEmailVerificationLink = async (user) => {
   const verificationToken = generateRandomToken();
   const verificationLink = `dummylink?token=${verificationToken}`;
 
   try {
     // await transporter.sendMail({
     //   from: `"Francel Boarding House" <${process.env.SMTP_USER}>`,
-    //   to: email,
+    //   to: user.email,
     //   subject: "Francel BHMS Landlord Email Verification",
     //   html: `<h2>Welcome to Francel BHMS!</h2>
     //          <p>Click the link below to verify your email. Expires in 3 minutes.</p>
@@ -16,7 +17,7 @@ export const sendEmailVerificationLink = async (email, user) => {
     //   `,
     // });
 
-    await cacheVerificationToken(verificationToken, landlord);
+    await cacheVerificationToken(verificationToken, user);
     return verificationToken; //for testing purposes
   } catch (error) {
     console.error("Error sending email verification link", error);
@@ -24,30 +25,31 @@ export const sendEmailVerificationLink = async (email, user) => {
   }
 };
 
-export const sendSetPasswordLink = async (
-  username,
-  setPasswordLink,
-  receiverEmail,
-  tenantName,
-) => {
+export const sendSetPasswordLink = async (email, user) => {
+  const setPasswordToken = generateRandomToken();
+  const setPasswordLink = `dummylink?token=${setPasswordToken}`;
+
   try {
-    await transporter.sendMail({
-      from: `"Francel Boarding House" <${process.env.SMTP_USER}>`,
-      to: receiverEmail,
-      subject: "New Tenant Account: Set Password",
-      html: `
-      <div style="font-family: Arial, sans-serif; line-height: 1.5; color: #333;">
-        <h2 style="color: #2E86C1;">New Tenant Account Created</h2>
-        <p>A new tenant account has been created for <strong>${tenantName}</strong>.</p>
+    // await transporter.sendMail({
+    //   from: `"Francel Boarding House" <${process.env.SMTP_USER}>`,
+    //   to: email,
+    //   subject: "New Tenant Account: Set Password",
+    //   html: `
+    //   <div style="font-family: Arial, sans-serif; line-height: 1.5; color: #333;">
+    //     <h2 style="color: #2E86C1;">New Tenant Account Created</h2>
+    //     <p>A new tenant account has been created for <strong>${user.firstName} ${user.lastName}</strong>.</p>
 
-        <p><strong>Tenant Username:</strong> ${username}</p>
+    //     <p><strong>Tenant Username:</strong> ${user.username}</p>
 
-        <p>Forward this link to the tenant so they can securely set their password:</p>
-        <p><a href="${setPasswordLink}" target="_blank" style="padding: 10px 15px; background-color: #2E86C1; color: #fff; text-decoration: none; border-radius: 5px;">Set Password</a></p>
-        <p>This link is valid for 24 hours.</p>
-      </div>
-    `,
-    });
+    //     <p>Forward this link to the tenant so they can securely set their password:</p>
+    //     <p><a href="${setPasswordLink}" target="_blank" style="padding: 10px 15px; background-color: #2E86C1; color: #fff; text-decoration: none; border-radius: 5px;">Set Password</a></p>
+    //     <p>This link is valid for 24 hours.</p>
+    //   </div>
+    // `,
+    // });
+
+    await cacheSetPasswordToken(setPasswordToken, user);
+    return setPasswordToken; //for testing purposes
   } catch (error) {
     console.error("Error sending set password link", error);
     throw error;
