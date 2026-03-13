d61d71d9ac3846447288527c5e2089609cc43de6
Francis Ge <francisge527@gmail.com>
2026-03-13 22:32:17 +0800

    add new route for getting posts

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { uploadPost } from "../controllers/post.controller.js";
+import { uploadPost, sendAllPost } from "../controllers/post.controller.js";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
 import { authorizeUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.post("/posts", authenticateUser, authorizeUser(["landlord"]), uploadPost);
+router.post(
+  "/posts",
+  authenticateUser,
+  authorizeUser(["landlord"]),
+  uploadPost,
+);
+
+router.get("/posts", authenticateUser, sendAllPost);
 
 export default router;
