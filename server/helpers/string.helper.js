e2058b3dd13f560f3c3c5113fea7d03a9ae581a9
Francis Ge <francisge527@gmail.com>
2026-03-13 23:38:13 +0800

    deleted files due to renaming server folder to 'backend'

--- server/helpers/string.helper.js
+++
@@ -1,8 +0,0 @@
-export const capitalizeWords = (string) => {
-  return string
-    .toLowerCase()
-    .trim()
-    .split(/\s+/)
-    .map((word) => word[0].toUpperCase() + word.slice(1))
-    .join(" ");
-};
