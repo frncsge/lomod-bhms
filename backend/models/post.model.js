2cc1a9913e2ed63fae4a3fa24e8283e165d5cdb6
Francis Ge <francisge527@gmail.com>
2026-03-13 23:30:19 +0800

    add query functions for getting a single post and updating post either by text or setting it to archive

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -21,3 +21,41 @@ export const getAllPost = async () => {
     throw error;
   }
 };
+
+export const getPost = async (id) => {
+  try {
+    const result = await pool.query("SELECT * FROM post WHERE post_id = $1", [
+      id,
+    ]);
+    return result.rows[0];
+  } catch (error) {
+    console.error("Error in getPost function", error);
+    throw error;
+  }
+};
+
+export const updatePost = {
+  text: async (id, newText) => {
+    try {
+      await pool.query("UPDATE post SET post_content = $1 WHERE post_id = $2", [
+        newText,
+        id,
+      ]);
+    } catch (error) {
+      console.error("Error in updatePost.text function", error);
+      throw error;
+    }
+  },
+
+  archive: async (id, archived) => {
+    try {
+      await pool.query("UPDATE post SET archive = $1 WHERE post_id = $2", [
+        archived,
+        id,
+      ]);
+    } catch (error) {
+      console.error("Error in updatePost.archive function", error);
+      throw error;
+    }
+  },
+};
