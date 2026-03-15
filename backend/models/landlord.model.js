b7aa55b6b8b0498111ee7a718a5b815b0a7880d4
Francis Ge <francisge527@gmail.com>
2026-03-15 15:04:28 +0800

    update getLandlordById funciton to getLandlordByUserId

--- backend/models/landlord.model.js
+++ backend/models/landlord.model.js
@@ -1,17 +1,15 @@
 import pool from "../config/db.config.js";
 
-const getLandlordById = async (id) => {
+export const getLandlordByUserId = async (id) => {
   try {
     const result = await pool.query(
-      `SELECT * FROM landlord WHERE landlord_id = $1`,
+      `SELECT * FROM landlord WHERE user_id = $1`,
       [id],
     );
 
     return result.rows[0];
   } catch (error) {
-    console.error("Error in getLandlordById function:", error);
+    console.error("Error in getLandlordByUserId function:", error);
     throw error;
   }
 };
-
-export { getLandlordById };
